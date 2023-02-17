f34a00d1e6f148834e3375a2ef2dd58a5e1f7940
chzhyang <ge.yang@intel.com>
2023-02-17 06:18:06 +0000

    update containerd config and installation script

--- crictl/part3.sh
+++ crictl/part3.sh
@@ -12,21 +12,23 @@ sudo ln -s /opt/kata/bin/kata-monitor /usr/bin/kata-monitor
 
 # install containerd
 wget https://github.com/containerd/containerd/releases/download/v1.6.16/cri-containerd-1.6.16-linux-amd64.tar.gz
-sudo tar --no-overwrite-dir -C / -xzf cri-containerd-1.6.16-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local cri-containerd-1.6.16-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+sudo mkdir -p usr/local/lib/systemd/system
+sudo cp containerd.service usr/local/lib/systemd/system/
+# sudo tar --no-overwrite-dir -C / -xzf cri-containerd-1.6.16-linux-amd64.tar.gz
 
 # install cni-plugin
 wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
-mkdir -p /opt/cni/bin
+sudo mkdir -p /opt/cni/bin
 sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
-
 
 # start containerd service
 sudo systemctl daemon-reload
 sudo systemctl enable --now containerd
 
-# config cni plugin
-# https://github.com/containernetworking/cni
-mkdir -p /etc/cni/net.d
+# config cni plugin https://github.com/containernetworking/cni
+sudo mkdir -p /etc/cni/net.d
 $ cat >/etc/cni/net.d/10-mynet.conf <<EOF
 {
 	"cniVersion": "0.2.0",
@@ -50,17 +52,22 @@ sudo mkdir /etc/containerd
 sudo sh -c "containerd config dump > /etc/containerd/config.toml" 
 sudo cp /etc/containerd/config.toml /etc/containerd/config.toml.orgin 
 # sudo vi /etc/containerd/config.toml ==============================================
-# sudo systemctl daemon-reload
-# sudo systemctl restart containerd
+wget https://raw.githubusercontent.com/chzhyang/coldstartup/main/crictl/containerd-config.toml
+sudo cp containerd-config.toml /etc/containerd/config.toml
+sudo systemctl restart containerd
 
 # config kata runtime plugin
-
-
 sudo rm configuration.toml
 sudo ln -s configuration-clh.toml configuration.toml
 
 
-
 # pull images
-
-sudo ctr -n k8s.io image pull gcr.io/knative-samples/helloworld-go
+sudo crictl pull registry.k8s.io/pause:3.6
+sudo crictl pull busybox
+# pull image using ctr into namespace k8s.io = crictl pull image
+# sudo ctr -n k8s.io image pull gcr.io/knative-samples/helloworld-go:latest
+# use crictl to run pod
+sudo crictl run container-config.json pod-config.json
+# list pods and containers
+sudo crictl pods
+sudo crictl ps -a
